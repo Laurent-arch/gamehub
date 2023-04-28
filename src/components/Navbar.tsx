02f6f56e211ef10839e6493de39fde68df1c7fda
laurent garcia <garcia.laurent@hotmail.fr>
2023-04-28 16:26:07 +0300

    added dark mode

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { HStack, Image, Text } from "@chakra-ui/react"
+import logo from '../assets/logo.webp'
+import ColorModeSwitch from "./ColorModeSwitch"
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <HStack>
+        <Image src={logo} boxSize='60px'/>
+        <ColorModeSwitch />
+    </HStack>
+  )
+}
+
+export default Navbar
